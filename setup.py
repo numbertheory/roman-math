29657434e09df0e63f57d35ed1e252dcc576ee79
JP Etcheber <jetcheber@gmail.com>
2019-05-27 12:02:47 -0700

    setup scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='roman_math',
+   version='1.0',
+   description='Roman numeral convertor and math functions.',
+   author='JP Etcheber',
+   author_email='jetcheber@gmail.com',
+   packages=['roman_math'],
+   use_scm_version=True,
+   setup_requires=['setuptools_scm']
+)
